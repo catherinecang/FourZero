68a157f09e6375f9df096eb9d78d555e2d360724
Catherine Cang <catherinecang@calvisitor-10-105-133-237.calvisitor.1918.berkeley.edu>
2017-09-13 15:14:34 -0700

    after decal

--- fourzero.py
+++ fourzero.py
@@ -1,12 +1,18 @@
+from enum import Enum 
+DWLT = Enum('DWLT', 'Draw Win Loss Tie')
 def solve(init_pos, primitive, generate_moves, do_move):
-	generate_moves = {1, 2}
-	if init_pos % 3 == 0:
-		return "Loss"
-	else:
-		return "Win"
+	if primitive(init_pos) == "Loss":
+		return False
+	a = 0
+	for i in generate_moves(init_pos):
+		b =solve(do_move(init_pos, i), primitive, generate_moves, do_move) 
+		a = a or not b
+	return a
+
+
 
 def generate_moves(pos):
-	assert pos > = 1
+	assert pos > 0
 	if pos < 2:
 		return [1]
 	else:
@@ -17,13 +23,21 @@ def do_move(pos, action):
 
 def primitive(pos):
 	if pos == 0:
-		return "LOSS"
+		return "Loss"
+	else:
+		return "Undecided"
+	"""if pos == 0:
+		return False
 	elif pos == 1 or pos == 2:
-		return "WIN"
+		return True
+	else:
+		return not(primitive(pos-1) and primitive(pos - 2))"""
 
 def main():
-	solve(4, primitive, generate_moves(), do_move)
+	if solve(4, primitive, generate_moves, do_move):
+		print("Win!")
+	else:
+		print("Loss")
+
 if __name__ == '__main__':
 	main()
-
-#hello i am testing github hello this is a change
