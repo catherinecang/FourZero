ab997416f7eee48554cd66f814f1039a12e6435f
Catherine Cang <catherinecang@calvisitor-10-105-182-62.calvisitor.1918.berkeley.edu>
2017-09-13 11:53:10 -0700

    Add file

--- fourzero.py
+++ fourzero.py
@@ -5,7 +5,23 @@ def solve(init_pos, primitive, generate_moves, do_move):
 	else:
 		return "Win"
 
+def generate_moves(pos):
+	assert pos > = 1
+	if pos < 2:
+		return [1]
+	else:
+		return [1,2]
+
+def do_move(pos, action):
+	return pos - action
+
+def primitive(pos):
+	if pos == 0:
+		return "LOSS"
+	elif pos == 1 or pos == 2:
+		return "WIN"
+
 def main(3, 5, 2, 1):
-	solve()
+	solve(4, primitive, generate_moves(), do_move)
 if __name__ == '__main__':
 	main()
