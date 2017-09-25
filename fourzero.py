4977a7ed844ba3dc055d6f5106b7428d78d48702
Catherine Cang <catherinecang@Catherines-MacBook-Pro.local>
2017-09-25 14:16:39 -0700

    ok

--- fourzero.py
+++ fourzero.py
@@ -1,30 +1,40 @@
 def solve(init_pos, primitive, generate_moves, do_move):
-	if primitive(init_pos) == "Loss":
-		return False
-	a = 0
-<<<<<<< HEAD
-	for i in generate_moves(init_pos):
-		b =solve(do_move(init_pos, i), primitive, generate_moves, do_move) 
-		a = a or not b
-	return a
-=======
-	count = 0 
-	for i in generate_moves(init_pos):
-		b = solve(do_move(init_pos, i), primitive, generate_moves, do_move) 
-		a = a or not b
-		count += 1
-	if a:
-		return "Win", count
-	else:
-		return "Loss", count
->>>>>>> 2
+	def tree(root, branches = []):
+		return [root, branches]
+	def root(t):
+		return t[0]
+	def branches(t):
+		return t[1]
+	def isleaf(t):
+		return not branches(t)
+	def shortestdistance(t):
+		if isleaf(t):
+			return 0
+		else:
+			return min([shortestdistance(b) for b in branches(t)]) + 1
+	def createtree(n):
+		if n < 0:
+			return tree(False)
+		elif primitive(n) == "Loss":
+			return tree(False)
+		else:
+			branches = [createtree(n-1), createtree(n-2)]
+			rt = True
+			for b in branches:
+				rt = rt and root(b)
+			rt = not rt
+			return tree(rt, branches)
+	soln =  createtree(init_pos)
+	return root(soln), shortestdistance(soln)
+	
+
 
 def generate_moves(pos):
 	assert pos > 0
 	if pos < 2:
 		return [1]
 	else:
-		return [1,2]
+		return [2,1]
 
 def do_move(pos, action):
 	return pos - action
@@ -36,16 +46,14 @@ def primitive(pos):
 		return "Undecided"
 
 def main():
-<<<<<<< HEAD
-	if solve(4, primitive, generate_moves, do_move):
-		print("Win!")
+	n = 4
+	if solve(4, primitive, generate_moves, do_move)[0]:
+		ans = "Win"
 	else:
-		print("Loss")
-=======
+		ans = "Loss"
 	print ("The amount of moves left equals how many moves per person (as in each time someone takes sticks that counts as a move).")
-	print(solve(4, primitive, generate_moves, do_move))
+	print(ans, solve(4, primitive, generate_moves, do_move)[1])
 
->>>>>>> 2
 
 if __name__ == '__main__':
 	main()
